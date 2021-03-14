9dee1d7f8830b57e78ae805af99b0822e48b2051
jayshrikathore <kathore777@gmail.com>
2021-03-14 19:22:32 +0530

    added file

--- src/main/java/beans/Car.java
+++ src/main/java/beans/Car.java
@@ -3,7 +3,10 @@ package beans;
 public class Car {
 int a=10;
 syso("20");
-hav epaientence one day your drem come true"
+
+have epaientence one day your drem come true"
+just trust your self
+keep learning
 
 	
 	
